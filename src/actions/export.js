bd2d71969684ffd2b31d287a83f75350e4dbfde5
Raimonds Zarins <raimondszarins2@gmail.com>
2017-11-27 14:34:11 +0200

    New export

--- src/actions/export.js
+++ src/actions/export.js
@@ -2,43 +2,91 @@ import { store } from "wabi"
 import { saveAs } from "../../libs/FileSaver"
 import { JSZip } from "../../libs/JSZip"
 import fs from "../fs/FileSystem"
+import Assets from "../actions/Assets"
 
-const exportZip = function() {
-	const assets = store.get("assets")
-	const id = store.get("settings.id")
-	const zip = new JSZip()
-	let count = Object.keys(assets).length + 1
-
-	fs.read("db.json", (error, contents) => {
-		zip.file("db.json", contents)
-		count--
-		if(count === 0) {
-			finishZip(zip)
-		}
-	})
-
-	for(const key in assets) {
-		const asset = assets[key]
-		const cut_location = asset.path.indexOf(id) + id.length + 1
-		const local_path = asset.path.substr(cut_location)
-
-		fs.readBlob(local_path, (error, contents) => {
-			const name = asset.path.split("/").pop()
-			zip.folder(asset.type.toLowerCase()).file(name, contents)
-			count--
-			if(count === 0) {
-				finishZip(zip)
+const exports = {
+	defaultOptions: {
+		type: "All", //All, JSON, Assets
+		format: "Compressed" //Compressed, Uncompressed
+	},
+	All: (assets) => {
+		return new Promise((resolve, reject) => {
+			Promise.all([exports.JSON(assets), exports.Assets(assets)]).then((values) => {
+				resolve(values[0].concat(values[1]))
+			})
+		})
+	},
+	JSON: (assets) => {
+		return new Promise((resolve, reject) => {
+			let processed_assets = Object.assign({}, assets)
+			Object.keys(processed_assets).forEach((key) => {
+				delete processed_assets[key]['cache']
+			})
+			delete processed_assets[""]
+			resolve([{
+				"name": "db.json",
+				"contents": JSON.stringify(processed_assets),
+				"type": "string"
+			}])
+		})
+	},
+	Assets: (assets) => {
+		return new Promise((resolve, reject) => {
+			let contents = []
+			for (var key in assets) {
+				if(assets[key].ext != null) {
+					contents.push(exports.GetContents(Assets.buildPath(assets[key])))
+				}
 			}
+
+			Promise.all(contents).then((values) => {
+				resolve(values)
+			})
+		})
+	},
+	GetContents: (path) => {
+		return new Promise((resolve, reject) => {
+			fs.readBlob(path, (error, contents) => {
+				resolve({
+					"name": contents.name,
+					"contents": contents,
+					"type": "blob"
+				})
+			})
+		})
+	},
+	Compressed: (files) => {
+		const zip = new JSZip()
+		for (var i = 0; i < files.length; i++) {
+			zip.file(files[i].name, files[i].contents)
+		}
+		zip.generateAsync({type:"blob"}).then(function (blob) {
+			saveAs(blob, store.get("project/name") + ".zip")
+		})
+	},
+	Uncompressed: (files) => {
+		for (var i = 0; i < files.length; i++) {
+			let blob = files[i].contents
+			if(files[i].type === "string") {
+				blob = new Blob([files[i].contents], {
+					type: "text/plain"
+				})
+			}		
+			saveAs(blob, files[i].name)
+		}
+	},
+	Download: (options) => {
+		options = Object.assign({}, exports.defaultOptions, options)
+		return new Promise((resolve, reject) => {
+			const assets = store.get("assets")
+			exports[options.type](assets).then((files) => {
+				exports[options.format](files)
+				resolve()
+			})
 		})
 	}
 }
 
-const finishZip = function(zip) {
-	zip.generateAsync({type:"blob"}).then(function (blob) {
-		saveAs(blob, store.get("settings.name") + ".zip")
-	})
-}
-
 export default {
-	exportZip
+	exports
 }
