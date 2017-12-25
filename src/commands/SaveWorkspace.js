fca15cd60168e9b5f73c871ba63be6422b2e269f
Tenjou-PC\Tenjou <arthur.der.shefer@gmail.com>
2017-12-25 10:14:17 +0200

    Fix workspace saving

--- src/commands/SaveWorkspace.js
+++ src/commands/SaveWorkspace.js
@@ -2,8 +2,8 @@ import Command from "~/Command"
 import Workspace from "~/system/Workspace"
 
 class SaveWorkspace extends Command {
-	execute() {
-		Workspace.save(this.param)
+	execute(param) {
+		Workspace.save(param)
 	}
 }
 
